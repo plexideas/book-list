ed269f7fc1bd2d4284202c68230d17c0d1874cf4
Sergei Sakharovskii <eightbitago@gmail.com>
2022-02-14 00:12:26 +0300

    App контейнер

---
+++ src/components/app/app.tsx
@@ -0,0 +1,48 @@
+import React, { VFC } from 'react';
+import { Tab, Tabs } from '../tabs/tabs';
+import { BookList } from '../book-list/book-list';
+import { useGetBooks } from '../../hooks/use-get-books';
+import { useSelector } from 'react-redux';
+import { selectFilters } from '../../features/filters-slice';
+
+export const App: VFC = () => {
+    const { isLoading, doneList, inProgressList, toReadList } = useGetBooks();
+
+    const filters = useSelector(selectFilters);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>BookList</h1>
+            <Tabs>
+                <Tab id="toread" title={`To read (${toReadList.length})`}>
+                    <BookList
+                        status="toread"
+                        books={toReadList.filter(book =>
+                            filters.every(tag => book.tags.includes(tag))
+                        )}
+                    />
+                </Tab>
+                <Tab id="inprogress" title={`In progress (${inProgressList.length})`}>
+                    <BookList
+                        status="inprogress"
+                        books={inProgressList.filter(book =>
+                            filters.every(tag => book.tags.includes(tag))
+                        )}
+                    />
+                </Tab>
+                <Tab id="done" title={`Done (${doneList.length})`}>
+                    <BookList
+                        status="done"
+                        books={doneList.filter(book =>
+                            filters.every(tag => book.tags.includes(tag))
+                        )}
+                    />
+                </Tab>
+            </Tabs>
+        </div>
+    );
+};
